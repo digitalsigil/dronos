1aba1837aec446335c346680a1cfcf9e3d73b9b3
RC Buchanan <cliff451@gmail.com>
2014-12-01 20:05:54 +0000

    changes

--- zedsw/bluGPIO.c
+++ zedsw/bluGPIO.c
@@ -1,14 +1,35 @@
+#include <ctype.h>
+#include <err.h>
+#include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
+#include <unistd.h>
+#include <termios.h>
+#include <time.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+#define FAST_TIMEOUT 3
+#define BLUART_DEV "/dev/ttyPS1"
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+
 
 struct gpio {
 	char	*root;
-}
+};
+
+
+int debug = 1;
+
 
 struct	gpio *initGPIO(char *);
 void	freeGPIO(struct gpio *);
 void	setupGPIOpin(int, int);
 void	writeGPIOpin(int, int);
+
 
 struct gpio *
 initGPIOpin(char *root)
@@ -22,7 +43,7 @@ initGPIOpin(char *root)
 	n = strlen(root) + 1;
 	if ((rv->root = malloc(n * sizeof (char))) == NULL)
 		goto error;
-	strcpy(rv->root, root, n);
+	strncpy(rv->root, root, n);
 	
 	return rv;
 
@@ -47,4 +68,123 @@ void
 setupGPIOpin(int pin, int dir)
 {
 	
+}
+
+int
+openBluART(char *dev)
+{
+	int	fd;
+	struct	termios ti;
+
+	if ((fd = open(dev, O_RDWR, 0)) == -1)
+		return -1;
+
+	if (tcgetattr(fd, &ti) == -1)
+		return -1;
+
+
+	ti.c_cflag |= CRTSCTS;
+
+	cfsetispeed(&ti, B115200);
+	cfsetospeed(&ti, B115200);
+	cfmakeraw(&ti);
+
+	if (tcsetattr(fd, TCSANOW, &ti) != 0)
+		return -1;
+
+	return fd;
+}
+
+int
+readStrBluART(int fd, char *s, time_t timeout)
+{
+	char	b;
+	char	*m;
+	time_t	t0;
+	
+	t0 = time(NULL);
+
+	if (debug) printf("readStrBluART: \"");
+	m = s;
+	while (*m != '\0') {
+		read(fd, &b, sizeof (b));
+		if (debug) printf(isgraph(b) ? "%c" : "\\%03o", b);
+
+		if (*m++ != b)
+			m = s;
+
+		if (t0 - time(NULL) > timeout) {
+			if (debug) printf("\" ... timout\n");
+			return -1;
+		}
+	}
+
+	if (debug) printf("\" ... match\n");
+	return 0;
+}
+
+void
+writeStrBluART(int fd, char *s)
+{
+	int	n;
+
+	n = strlen(s);
+	while (n > 0)	n -= write(fd, s, n);
+}
+
+int
+scanDevBluART(int fd, char *d, int scantime)
+{
+	int found = 0;
+
+	writeStrBluART(fd, "F\n");
+	if (readStrBluART(fd, "AOK", FAST_TIMEOUT) != 0)
+		return -1;
+
+	if (readStrBluART(fd, d, scantime) == 0)
+		found = 1;
+
+	writeStrBluART(fd, "X\n");
+	if (readStrBluART(fd, "AOK", FAST_TIMEOUT) != 0)
+		return -1;
+
+	return found ? 0 : -1;
+}
+
+int
+connectDevBluART(int fd, char *d)
+{
+	int found = 0;
+
+
+	if (readStrBluART(fd, d, timeout) == 0)
+		found = 1;
+
+	writeStrBluART(fd, "X\n");
+	if (readStrBluART(fd, "AOK", FAST_TIMEOUT) != 0)
+		return -1;
+
+	return found ? 0 : -1;
+}
+
+int
+main(int argc, char **argv)
+{
+	int	fd;
+
+	if ((fd = openBluART(BLUART_DEV)) == -1)
+		err(1, "Couldn't open" BLUART_DEV);
+
+	writeStrBluART(fd, "+\n");
+	readStrBluART(fd, "Echo", 5);
+	writeStrBluART(fd, "SF,1\n");
+	readStrBluART(fd, "AOK", 5);
+	writeStrBluART(fd, "SS,C0000000\n");
+	readStrBluART(fd, "AOK", 5);
+	writeStrBluART(fd, "SR,92000000\n");
+	readStrBluART(fd, "AOK", 5);
+	writeStrBluART(fd, "R,1\n");
+	readStrBluART(fd, "CMD", 5);
+
+	return 0;
 }
