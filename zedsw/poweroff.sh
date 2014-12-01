ffe57c86056c13f746aa52f63c18daaedb08b082
RC Buchanan <cliff451@gmail.com>
2014-12-01 20:07:49 +0000

    added shell scripts

---
+++ zedsw/poweroff.sh
@@ -0,0 +1,6 @@
+echo 90 > /sys/class/gpio/export
+echo 86 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio90/direction 
+echo out > /sys/class/gpio/gpio86/direction 
+echo 0 > /sys/class/gpio/gpio90/value 
+echo 0 > /sys/class/gpio/gpio86/value 
